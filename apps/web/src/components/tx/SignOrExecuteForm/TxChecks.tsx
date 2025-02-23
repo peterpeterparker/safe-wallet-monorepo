0f2209b4e3851b11b3f8f55fadd499263b1feafe
Aaron Cook <aaron@safe.global>
2025-02-23 10:09:52 +0100

    feat: include link to third-party hash checking tool

--- apps/web/src/components/tx/SignOrExecuteForm/TxChecks.tsx
+++ apps/web/src/components/tx/SignOrExecuteForm/TxChecks.tsx
@@ -1,16 +1,26 @@
 import { isTxSimulationEnabled } from '@/components/tx/security/tenderly/utils'
 import { useCurrentChain, useHasFeature } from '@/hooks/useChains'
 import { FEATURES } from '@/utils/chains'
-import { type ReactElement, useContext } from 'react'
+import { type ReactElement } from 'react'
 import { TxSimulation, TxSimulationMessage } from '@/components/tx/security/tenderly'
 import TxCard from '@/components/tx-flow/common/TxCard'
-import { SafeTxContext } from '@/components/tx-flow/SafeTxProvider'
-import { Box, Typography } from '@mui/material'
+import { Alert, Box, Typography } from '@mui/material'
+import ExternalLink from '@/components/common/ExternalLink'
+import type { SafeTransaction, MetaTransactionData } from '@safe-global/safe-core-sdk-types'
 
 import css from './styles.module.css'
 
-const TxChecks = ({ executionOwner }: { executionOwner?: string }): ReactElement | null => {
-  const { safeTx } = useContext(SafeTxContext)
+const SAFE_TX_HASHES_UTIL_URL = 'https://www.safehashpreview.com/'
+
+const TxChecks = ({
+  executionOwner,
+  disabled = false,
+  transaction,
+}: {
+  executionOwner?: string
+  disabled?: boolean
+  transaction: SafeTransaction | Array<MetaTransactionData>
+}): ReactElement | null => {
   const chain = useCurrentChain()
   const isRiskMitigationFeatureEnabled = useHasFeature(FEATURES.RISK_MITIGATION)
   const isTxSimulationFeatureEnabled = isTxSimulationEnabled(chain)
@@ -23,7 +33,12 @@ const TxChecks = ({ executionOwner }: { executionOwner?: string }): ReactElement
     <TxCard>
       <Typography variant="h5">Transaction checks</Typography>
 
-      <TxSimulation disabled={false} transactions={safeTx} executionOwner={executionOwner} />
+      <Alert severity="info">
+        We strongly advise verifying your transaction with a third-party tool like{' '}
+        <ExternalLink href={SAFE_TX_HASHES_UTIL_URL}>Safe Hash Preview</ExternalLink> to ensure its authenticity.
+      </Alert>
+
+      <TxSimulation disabled={disabled} transactions={transaction} executionOwner={executionOwner} />
 
       <Box className={css.mobileTxCheckMessages}>
         <TxSimulationMessage />
